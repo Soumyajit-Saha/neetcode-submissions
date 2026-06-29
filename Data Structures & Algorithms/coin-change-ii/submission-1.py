9e809d7cf99bdb13f137dd63eb1d13ebfdd10912
Soumyajit Saha <57801484+Soumyajit-Saha@users.noreply.github.com>
2026-06-29 12:48:38 -0500

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        coins.sort()
+
+        for i in range(len(coins)):
+            for j in range(1, amount + 1):
+                if coins[i] > j:
+                    continue
+                dp[j] += dp[j - coins[i]]
+
+        print(dp)
+        return dp[-1]
